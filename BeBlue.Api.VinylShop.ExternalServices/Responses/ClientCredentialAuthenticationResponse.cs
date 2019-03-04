9520e856e383a85936d47e0a4dd710e0e69e2b3d
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-03 21:46:15 -0300

    Created spotify client with method to retrieve albums for each genre

---
+++ BeBlue.Api.VinylShop.ExternalServices/Responses/ClientCredentialAuthenticationResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeBlue.Api.VinylShop.ExternalServices.Responses
+{
+	internal class ClientCredentialAuthenticationResponse
+	{
+		[JsonProperty(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+		public string AccessToken { get; set; }
+
+		[JsonProperty(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+		public int ExpiresIn { get; set; }
+
+		[JsonProperty(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+		public string Scope { get; set; }
+
+		[JsonProperty(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+		public string TokenType { get; set; }
+	}
+}
---
+++ BeBlue.Api.VinylShop.ExternalServices/SpotifyClient.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BeBlue.Api.VinylShop.DomainModel;
+using BeBlue.Api.VinylShop.ExternalServices.Responses;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BeBlue.Api.VinylShop.ExternalServices
+{
+	public class SpotifyClient : ISpotifyClient
+	{
+		private readonly HttpClient client;
+		private readonly SpotifySettings spotifySettings;
+
+		public SpotifyClient(HttpClient httpClient, SpotifySettings spotifySettings)
+		{
+			this.client = httpClient;
+			this.spotifySettings = spotifySettings;
+			this.Authenticate().GetAwaiter().GetResult();
+		}
+
+		private async Task Authenticate()
+		{
+			try
+			{
+				string credentials = this.ClientCredentialsAsBase64();
+
+				var parameters = new[] { new KeyValuePair<string, string>("grant_type", "client_credentials") };
+
+				this.client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(AuthenticationSchemes.Basic.ToString(), credentials);
+
+				var response = await this.client.PostAsync(this.spotifySettings.TokenUri, new FormUrlEncodedContent(parameters));
+
+				response.EnsureSuccessStatusCode();
+
+				var clientAuthentication = JsonConvert.DeserializeObject<ClientCredentialAuthenticationResponse>(await response.Content.ReadAsStringAsync());
+
+				this.SetAuthenticationTokenHeader(clientAuthentication);
+			}
+			catch (Exception e)
+			{
+				//TODO Log exception here
+			}
+		}
+
+		private void SetAuthenticationTokenHeader(ClientCredentialAuthenticationResponse clientAuthentication)
+		{
+			this.client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(clientAuthentication.TokenType, clientAuthentication.AccessToken);
+		}
+
+		private string ClientCredentialsAsBase64()
+		{
+			var credentialBytes = Encoding.UTF8.GetBytes($"{this.spotifySettings.ClientId}:{this.spotifySettings.Secret}");
+			return Convert.ToBase64String(credentialBytes);
+		}
+
+		public async Task<IReadOnlyList<AlbumResponse>> RetrieveAlbums()
+		{
+			var albums = new List<AlbumResponse>();
+			foreach (Genres genre in Enum.GetValues(typeof(Genres)))
+			{
+				var response = await this.client.GetAsync(this.spotifySettings.SearchUri + $"?q={genre}&genre:{genre}&type=track&limit=50");
+				var queryResult = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+				foreach (var album in queryResult["tracks"]["items"].Children().Select(x => x["album"].ToObject<AlbumResponse>()))
+				{
+					album.Genre = genre;
+					albums.Add(album);
+				}
+			}
+			return albums;
+		}
+	}
+}
