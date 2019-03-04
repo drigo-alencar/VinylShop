b89374cd69134c9e9e37a4235d3a41cef63b62e7
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-03 21:53:08 -0300

    Update startup class wiring all dependencies on dependency injection

--- BeBlue.Api.VinylShop.Presentation/Startup.cs
+++ BeBlue.Api.VinylShop.Presentation/Startup.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using BeBlue.Api.VinylShop.ExternalServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using BeBlue.Api.VinylShop.DataLayer;
 
 namespace BeBlue.Api.VinylShop.Presentation
 {
@@ -25,7 +29,23 @@ namespace BeBlue.Api.VinylShop.Presentation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
+
+			var mongoSettings = this.Configuration.GetSection(nameof(MongoSettings)).Get<MongoSettings>();
+			var spotifySettings = this.Configuration.GetSection(nameof(SpotifySettings)).Get<SpotifySettings>();
+
+			services.AddSingleton(mongoSettings);
+			services.AddSingleton(spotifySettings);
+
+			services.AddSingleton<HttpClient>();
+			services.AddSingleton<ISpotifyClient, SpotifyClient>();
+			services.AddSingleton<IMongoContext, MongoContext>();
+			services.AddSingleton<IUnitOfWork, UnitOfWork>();
+
+			services.AddSingleton<CatalogBootStrapper>();
+
+
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +59,8 @@ namespace BeBlue.Api.VinylShop.Presentation
             {
                 app.UseHsts();
             }
+
+			app.ApplicationServices.GetRequiredService<CatalogBootStrapper>();
 
             app.UseHttpsRedirection();
             app.UseMvc();
