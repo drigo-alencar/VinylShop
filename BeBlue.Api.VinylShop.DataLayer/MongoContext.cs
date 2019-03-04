110227ef42492d594df2b9b96ff1a007381ddbd2
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-03 21:49:55 -0300

    Create mongo context and settings

---
+++ BeBlue.Api.VinylShop.DataLayer/MongoContext.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace BeBlue.Api.VinylShop.DataLayer
+{
+	public class MongoContext : IMongoContext
+	{
+		public MongoContext(MongoSettings mongoSettings)
+		{
+			var mongoClient = new MongoClient($"mongodb://{mongoSettings.Host}:{mongoSettings.Port}");
+			this.Database = mongoClient.GetDatabase(mongoSettings.Database);
+		}
+
+		public IMongoDatabase Database { get; }
+	}
+}
