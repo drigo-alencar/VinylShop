e41204d36fce874dbcbf8f6f75180d19dd9e3dca
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-03 21:51:00 -0300

    Create unit of work with reference to albums repository

---
+++ BeBlue.Api.VinylShop.DataLayer/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using BeBlue.Api.VinylShop.DataLayer.Repositories;
+
+namespace BeBlue.Api.VinylShop.DataLayer
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly IMongoContext context;
+
+		public UnitOfWork(IMongoContext context)
+		{
+			if (context == null) { throw new System.ArgumentNullException(nameof(context)); }
+			this.context = context;
+		}
+
+		private IAlbumsRepository albumsRepository;
+
+		public IAlbumsRepository AlbumsRepository
+		{
+			get
+			{
+				if (this.albumsRepository == null) { this.albumsRepository = new AlbumsRepository(this.context.Database); }
+				return this.albumsRepository;
+			}
+		}
+	}
+}
