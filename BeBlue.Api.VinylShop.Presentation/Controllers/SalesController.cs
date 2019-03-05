5735e08523e10b0507b18b53f3e5d3379f51321a
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-05 18:45:17 -0300

    Implent [GET] on [SalesController] accordingly with tests

--- BeBlue.Api.VinylShop.Presentation/Controllers/SalesController.cs
+++ BeBlue.Api.VinylShop.Presentation/Controllers/SalesController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeBlue.Api.VinylShop.DataLayer;
+using BeBlue.Api.VinylShop.DomainModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BeBlue.Api.VinylShop.Presentation.Controllers
@@ -10,5 +11,28 @@ namespace BeBlue.Api.VinylShop.Presentation.Controllers
 	[ApiController]
 	public class SalesController : Controller
 	{
+		private readonly IUnitOfWork unitOfWork;
+
+		public SalesController(IUnitOfWork unitOfWork)
+		{
+			this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+		}
+
+		[HttpGet("{id}", Name = "GetSaleById")]
+		public async Task<ActionResult<Sale>> Get(string id)
+		{
+			if (String.IsNullOrWhiteSpace(id)) { return this.BadRequest(BadRequestMessages.MustProvideSaleId); }
+
+			try
+			{
+				var sale = await this.unitOfWork.SalesRepository.GetByIdAsync(id);
+
+				return this.Ok(sale);
+			}
+			catch (Exception e)
+			{
+				return this.StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+			}
+		}
 	}
 }
