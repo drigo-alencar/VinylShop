d58d88a08ead4dfb9f854fcf1e2b81393637ee39
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-05 17:41:38 -0300

    Create [SaleRepository] enabling to save a sale to database

---
+++ BeBlue.Api.VinylShop.DataLayer/Repositories/ISalesRepository.cs
@@ -0,0 +1,10 @@
+﻿using BeBlue.Api.VinylShop.DomainModel;
+using System.Threading.Tasks;
+
+namespace BeBlue.Api.VinylShop.DataLayer.Repositories
+{
+	public interface ISalesRepository
+	{
+		Task Save(Sale sale);
+	}
+}
---
+++ BeBlue.Api.VinylShop.DataLayer/Repositories/SalesRepository.cs
@@ -0,0 +1,21 @@
+﻿using BeBlue.Api.VinylShop.DomainModel;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace BeBlue.Api.VinylShop.DataLayer.Repositories
+{
+	public class SalesRepository : ISalesRepository
+	{
+		private IMongoDatabase database;
+
+		public SalesRepository(IMongoDatabase database)
+		{
+			this.database = database;
+		}
+
+		public async Task Save(Sale sale)
+		{
+			await this.database.GetCollection<Sale>("Sales").InsertOneAsync(sale);
+		}
+	}
+}
