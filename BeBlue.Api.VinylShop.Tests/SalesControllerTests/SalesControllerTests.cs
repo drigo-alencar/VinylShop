a51ad622c9ec5c737e8d173a69a08185b5d62945
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-05 18:08:45 -0300

    Create [SalesController]

---
+++ BeBlue.Api.VinylShop.Tests/SalesControllerTests/SalesControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using BeBlue.Api.VinylShop.Presentation.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeBlue.Api.VinylShop.Tests.SalesControllerTests
+{
+	public class SalesControllerTests
+	{
+		protected SalesController controller;
+
+		public SalesControllerTests()
+		{
+			this.controller = new SalesController();
+		}
+	}
+}
