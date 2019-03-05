5cf7963a71283dbf9bf1b340fef287c1b21766f9
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-05 16:21:44 -0300

    Update cashback settings repository with a method to get the cashback setting of a genre

--- BeBlue.Api.VinylShop.DataLayer/Repositories/CashbackSettingsRepository.cs
+++ BeBlue.Api.VinylShop.DataLayer/Repositories/CashbackSettingsRepository.cs
@@ -1,8 +1,9 @@
-﻿using BeBlue.Api.VinylShop.DomainModel;
+using BeBlue.Api.VinylShop.DomainModel;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,13 @@ namespace BeBlue.Api.VinylShop.DataLayer.Repositories
 		public async Task BulkInsertAsync(IList<GenreCashbackSettings> genresCashbackSettings)
 		{
 			await this.database.GetCollection<GenreCashbackSettings>("CashbackSettings").InsertManyAsync(genresCashbackSettings);
+		}
+
+		public async Task<GenreCashbackSettings> GetByGenre(Genres genre)
+		{
+			var filter = Builders<GenreCashbackSettings>.Filter.Eq(g => g.Genre, genre);
+
+			return await this.database.GetCollection<GenreCashbackSettings>("CashbackSettings").Find(filter).FirstOrDefaultAsync();
 		}
 	}
 }
