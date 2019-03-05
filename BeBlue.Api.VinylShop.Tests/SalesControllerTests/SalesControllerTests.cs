2e2c33444176cb1bf9625b4fc0798b9044b04eb8
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-05 18:55:21 -0300

    Create test if try to create [SalesController] without [IUnitOfWork]

--- BeBlue.Api.VinylShop.Tests/SalesControllerTests/SalesControllerTests.cs
+++ BeBlue.Api.VinylShop.Tests/SalesControllerTests/SalesControllerTests.cs
@@ -1,17 +1,37 @@
-﻿using BeBlue.Api.VinylShop.Presentation.Controllers;
+﻿using AutoFixture;
+using BeBlue.Api.VinylShop.DataLayer;
+using BeBlue.Api.VinylShop.Presentation.Controllers;
+using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace BeBlue.Api.VinylShop.Tests.SalesControllerTests
 {
 	public class SalesControllerTests
 	{
 		protected SalesController controller;
+		protected Fixture fixture;
+		protected IUnitOfWork unitOfWork;
 
 		public SalesControllerTests()
 		{
-			this.controller = new SalesController();
+			this.fixture = new Fixture();
+
+			this.unitOfWork = Substitute.For<IUnitOfWork>();
+
+			this.controller = new SalesController(this.unitOfWork);
+		}
+
+		[Fact]
+		public void Attempt_to_create_sales_controller_without_unit_of_work_must_throw_an_argument_null_exception()
+		{
+			//Arrange -> Act
+			var result = Record.Exception(() => new SalesController(null));
+
+			//Assert
+			Assert.IsType<ArgumentNullException>(result);
 		}
 	}
 }
