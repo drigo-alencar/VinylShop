8dcfe859d4cd9308778374e175b24058b6e3f119
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-05 17:43:13 -0300

    Update [Sale] class with total cashback, total price, albums list and id properties

--- BeBlue.Api.VinylShop.DomainModel/Sale.cs
+++ BeBlue.Api.VinylShop.DomainModel/Sale.cs
@@ -1,6 +1,23 @@
-﻿namespace BeBlue.Api.VinylShop.DomainModel
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System.Collections.Generic;
+
+namespace BeBlue.Api.VinylShop.DomainModel
 {
 	public class Sale
 	{
+		public Sale()
+		{
+			this.Albums = new List<Album>();
+		}
+
+		[BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+		public string Id { get; set; }
+
+		public IList<Album> Albums { get; set; }
+
+		public double TotalCashback { get; set; }
+
+		public double TotalPrice { get; set; }
 	}
 }
