b394b4f0f21eab0de960f7023467a14543b38288
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-04 23:12:03 -0300

    Create [GenreCashbackSettings] to represent each cashback setting  that is available for each genre

---
+++ BeBlue.Api.VinylShop.DomainModel/GenreCashbackSettings.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeBlue.Api.VinylShop.DomainModel
+{
+	public class GenreCashbackSettings
+	{
+		[BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+		public string Id { get; set; }
+
+		[BsonRepresentation(BsonType.String)]
+		[JsonConverter(typeof(StringEnumConverter))]
+		public Genres Genre { get; set; }
+
+		public IList<Cashback> Cashbacks { get; set; }
+	}
+}
