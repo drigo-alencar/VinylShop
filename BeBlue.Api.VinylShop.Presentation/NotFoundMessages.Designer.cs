c7fa07ad0215a5a6eafb69bb53c4d59e5ad808d9
Rodrigo Alencar <rodrigo.alencar.sm@gmail.com>
2019-03-05 16:30:53 -0300

    Update bad request messages resource and create not found messages resource

--- BeBlue.Api.VinylShop.Presentation/NotFoundMessages.Designer.cs
+++ BeBlue.Api.VinylShop.Presentation/NotFoundMessages.Designer.cs
@@ -22,14 +22,14 @@ namespace BeBlue.Api.VinylShop.Presentation {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class BadRequestMessages {
+    public class NotFoundMessages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal BadRequestMessages() {
+        internal NotFoundMessages() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace BeBlue.Api.VinylShop.Presentation {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BeBlue.Api.VinylShop.Presentation.BadRequestMessages", typeof(BadRequestMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BeBlue.Api.VinylShop.Presentation.NotFoundMessages", typeof(NotFoundMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,38 +61,11 @@ namespace BeBlue.Api.VinylShop.Presentation {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to A cart must have a list with wished albums id&apos;s  to buy.
+        ///   Looks up a localized string similar to One or more albums can&apos;t be found, their id(s) are: {0}.
         /// </summary>
-        public static string CartMustHaveAListWithAlbumsIds {
+        public static string OneOrMoreAlbumsCantBeFound {
             get {
-                return ResourceManager.GetString("CartMustHaveAListWithAlbumsIds", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to A request to create a cart can&apos;t be null.
-        /// </summary>
-        public static string CartRequestCantBeNull {
-            get {
-                return ResourceManager.GetString("CartRequestCantBeNull", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Album Id can&apos;t be null or white space.
-        /// </summary>
-        public static string MustProvideAlbumId {
-            get {
-                return ResourceManager.GetString("MustProvideAlbumId", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Genre can&apos;t be null  or whtie space.
-        /// </summary>
-        public static string MustProvideGenre {
-            get {
-                return ResourceManager.GetString("MustProvideGenre", resourceCulture);
+                return ResourceManager.GetString("OneOrMoreAlbumsCantBeFound", resourceCulture);
             }
         }
     }
